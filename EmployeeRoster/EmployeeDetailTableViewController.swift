ebe754465a93a28d52639e035600d081a51c61d9
Ben Reeps <benreeps@gmail.com>
2020-12-10 16:53:52 -0500

    add date picker logic

--- EmployeeRoster/EmployeeDetailTableViewController.swift
+++ EmployeeRoster/EmployeeDetailTableViewController.swift
@@ -10,8 +10,17 @@ class EmployeeDetailTableViewController: UITableViewController, UITextFieldDeleg
     @IBOutlet weak var nameTextField: UITextField!
     @IBOutlet weak var dobLabel: UILabel!
     @IBOutlet weak var employeeTypeLabel: UILabel!
+    @IBOutlet weak var dobDatePicker: UIDatePicker!
     
     var employee: Employee?
+    
+    var isEditingBirthday = false {
+        didSet {
+            dobDatePicker.isHidden = !isEditingBirthday
+            tableView.beginUpdates()
+            tableView.endUpdates()
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -36,7 +45,7 @@ class EmployeeDetailTableViewController: UITableViewController, UITextFieldDeleg
     
     @IBAction func saveButtonTapped(_ sender: Any) {
         if let name = nameTextField.text {
-            employee = Employee(name: name, dateOfBirth: Date(), employeeType: .exempt)
+            employee = Employee(name: name, dateOfBirth: dobDatePicker.date, employeeType: .exempt)
             performSegue(withIdentifier: PropertyKeys.unwindToListIndentifier, sender: self)
         }
     }
@@ -51,5 +60,37 @@ class EmployeeDetailTableViewController: UITableViewController, UITextFieldDeleg
     func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
         return false
     }
-
+    // MARK: - Tableview delegates
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        if indexPath.row == 2 {
+            return isEditingBirthday ? 216.0 : 0.0
+        }
+        return 44.0
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        if indexPath.row == 1 {
+            isEditingBirthday = !isEditingBirthday
+            
+            dobLabel.textColor = .black
+            dobLabel.text = formatDate(date: dobDatePicker.date)
+        }
+    }
+    
+    // MARK: - Date Picker Functionality
+    
+    @IBAction func dateChanged(_ sender: Any) {
+        dobLabel.text = formatDate(date: dobDatePicker.date)
+        
+    }
+    
+    
+    func formatDate(date: Date) -> String {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .medium
+        
+        return formatter.string(from: date)
+    }
 }
