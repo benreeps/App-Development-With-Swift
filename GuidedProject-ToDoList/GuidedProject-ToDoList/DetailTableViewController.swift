28ab41ea9183ebc69d97b5fda8cb556ca5a29fcb
Ben Reeps <benreeps@gmail.com>
2020-12-12 18:19:48 -0500

    change fonts

---
+++ GuidedProject-ToDoList/GuidedProject-ToDoList/DetailTableViewController.swift
@@ -0,0 +1,13 @@
+//
+//  DetailTableViewController.swift
+//  GuidedProject-ToDoList
+//
+//  Created by Benjamin Reeps on 12/12/20.
+//
+
+import Foundation
+import UIKit
+
+class DetailTableViewController: UITableViewController {
+    
+}
