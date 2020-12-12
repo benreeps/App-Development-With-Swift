fd3ef9308ed7d4c570a76eee46eb9e4a70364456
Ben Reeps <benreeps@gmail.com>
2020-12-12 14:19:14 -0500

    add initial tabelView configuration for reusabel cell git status q clear exit

--- GuidedProject-ToDoList/GuidedProject-ToDoList/ToDoTableViewController.swift
+++ GuidedProject-ToDoList/GuidedProject-ToDoList/ToDoTableViewController.swift
@@ -10,4 +10,23 @@ import UIKit
 
 class ToDoTableViewController: UITableViewController {
     
+    var todos = [ToDo]()
+    
+    
+    
+    
+    // MARK:- TableView Data
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return todos.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoCell") else {
+            fatalError("Could not dequeue cell")
+        }
+        let todo = todos[indexPath.row]
+        cell.textLabel?.text = todo.title
+        return cell 
+    }
 }
