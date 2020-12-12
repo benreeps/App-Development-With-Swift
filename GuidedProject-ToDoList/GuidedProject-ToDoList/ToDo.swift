8422b0c7a603e1426729738c101334ac7de8f7dc
Ben Reeps <benreeps@gmail.com>
2020-12-12 13:19:21 -0500

    create and define the model

---
+++ GuidedProject-ToDoList/GuidedProject-ToDoList/ToDo.swift
@@ -0,0 +1,15 @@
+//
+//  ToDo.swift
+//  GuidedProject-ToDoList
+//
+//  Created by Benjamin Reeps on 12/12/20.
+//
+
+import Foundation
+
+struct ToDo {
+    var title: String
+    var isComplete: Bool
+    var dueDate: Date
+    var notes: String? 
+}
