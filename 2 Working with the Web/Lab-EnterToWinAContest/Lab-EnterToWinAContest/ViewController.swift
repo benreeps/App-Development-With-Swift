d62244ae1282918a80bff9263668f076a28c6934
Ben Reeps <benreeps@gmail.com>
2021-01-05 18:05:32 -0500

    animated email text view

--- 2 Working with the Web/Lab-EnterToWinAContest/Lab-EnterToWinAContest/ViewController.swift
+++ 2 Working with the Web/Lab-EnterToWinAContest/Lab-EnterToWinAContest/ViewController.swift
@@ -10,27 +10,6 @@ import UIKit
 class ViewController: UIViewController {
     var emailList: [Email] = []
     
-    var emailIsValid = false {
-        didSet {
-            if emailIsValid {
-                return
-                
-            } else {
-                let buttonBackgorund: UIView
-                let translateTransform = CGAffineTransform(translationX: 20, y: 0)
-                buttonBackgorund = emailTextField
-                
-                UIView.animate(withDuration: 0.25, animations: {
-                    self.emailTextField.transform = translateTransform
-                }) { (_) in UIView.animate(withDuration: 0.25, animations: {
-                    buttonBackgorund.transform = CGAffineTransform.identity
-                    })
-                
-                }
-            }
-        }
-    }
-    
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var submitButton: UIButton!
     
@@ -38,23 +17,48 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         submitButton.layer.cornerRadius = 22.0
-        
+        emailTextField.layer.borderColor = UIColor.lightGray.cgColor
+        emailTextField.layer.borderWidth = 0.3
+        emailTextField.layer.cornerRadius = 17
+        emailTextField.clipsToBounds = true
         
     }
     
     @IBAction func submitButtonPressed(_ sender: UIButton) {
         checkEmailStatus()
-        
+        emailTextField.resignFirstResponder()
+
+    }
+    
+    @IBAction func isEditingEmail(_ sender: UITextField) {
+        emailTextField.layer.borderColor = UIColor.lightGray.cgColor
+        emailTextField.layer.borderWidth = 0.3
+        emailTextField.layer.cornerRadius = 17
         
     }
     
+    
     func checkEmailStatus() {
-        if emailTextField.text?.contains("@" + ".com") != nil {
-            emailIsValid = !emailIsValid
+        let email = self.emailTextField.text
+        let emailView: UIView = self.emailTextField
+        
+        if email!.range(of: "@") != nil {
+            return
+            
         } else {
-            emailIsValid = !emailIsValid
+            let animation = CABasicAnimation(keyPath: "position")
+            animation.duration = 0.07
+            animation.repeatCount = 4
+            animation.autoreverses = true
+            animation.fromValue = NSValue(cgPoint: CGPoint(x: emailView.center.x - 10, y: emailView.center.y))
+            animation.toValue = NSValue(cgPoint: CGPoint(x: emailView.center.x + 10, y: emailView.center.y))
+            
+            emailView.layer.borderWidth = 1
+            emailView.layer.borderColor = UIColor.red.cgColor
+            emailView.layer.add(animation, forKey: "position")
+            
         }
     }
     
+   
 }
-
