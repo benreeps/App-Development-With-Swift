8a3d014c2cfae396feb8ddeb55dbafe8f65acb61
Ben Reeps <benreeps@gmail.com>
2021-01-04 23:25:42 -0500

    animate email text field

--- 2 Working with the Web/Lab-EnterToWinAContest/Lab-EnterToWinAContest/ViewController.swift
+++ 2 Working with the Web/Lab-EnterToWinAContest/Lab-EnterToWinAContest/ViewController.swift
@@ -8,8 +8,29 @@
 import UIKit
 
 class ViewController: UIViewController {
-    var emailIsValid = false
-
+    var emailList: [Email] = []
+    
+    var emailIsValid = false {
+        didSet {
+            if emailIsValid {
+                return
+                
+            } else {
+                let buttonBackgorund: UIView
+                let translateTransform = CGAffineTransform(translationX: 20, y: 0)
+                buttonBackgorund = emailTextField
+                
+                UIView.animate(withDuration: 0.25, animations: {
+                    self.emailTextField.transform = translateTransform
+                }) { (_) in UIView.animate(withDuration: 0.25, animations: {
+                    buttonBackgorund.transform = CGAffineTransform.identity
+                    })
+                
+                }
+            }
+        }
+    }
+    
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var submitButton: UIButton!
     
@@ -22,16 +43,18 @@ class ViewController: UIViewController {
     }
     
     @IBAction func submitButtonPressed(_ sender: UIButton) {
+        checkEmailStatus()
+        
         
     }
     
     func checkEmailStatus() {
         if emailTextField.text?.contains("@" + ".com") != nil {
-            emailIsValid = true
+            emailIsValid = !emailIsValid
         } else {
-            emailIsValid = false
+            emailIsValid = !emailIsValid
         }
     }
-
+    
 }
 
