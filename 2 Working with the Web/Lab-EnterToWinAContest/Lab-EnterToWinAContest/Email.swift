d53d47997d56658767d5d392c2c5dd9dacb3e9c7
Ben Reeps <benreeps@gmail.com>
2021-01-04 21:55:18 -0500

    start a new project/ set up storyboard

---
+++ 2 Working with the Web/Lab-EnterToWinAContest/Lab-EnterToWinAContest/Email.swift
@@ -0,0 +1,15 @@
+//
+//  Email.swift
+//  Lab-EnterToWinAContest
+//
+//  Created by Benjamin Reeps on 1/4/21.
+//
+
+import Foundation
+import UIKit
+
+struct Email {
+    var emailAddress: String?
+    
+  
+}
---
+++ 2 Working with the Web/Lab-EnterToWinAContest/Lab-EnterToWinAContest/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  Lab-EnterToWinAContest
+//
+//  Created by Benjamin Reeps on 1/4/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    var emailIsValid = false {
+       
+    }
+
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var submitButton: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        submitButton.layer.cornerRadius = 22.0
+        
+        
+    }
+
+
+}
+
