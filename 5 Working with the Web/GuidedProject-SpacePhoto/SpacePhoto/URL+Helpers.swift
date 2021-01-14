b45def9839b2aa04b1351f9b871d788397463317
Ben Reeps <benreeps@gmail.com>
2021-01-13 22:23:56 -0500

    add url helpers file

---
+++ 5 Working with the Web/GuidedProject-SpacePhoto/SpacePhoto/URL+Helpers.swift
@@ -0,0 +1,16 @@
+//
+//  URL+Helpers.swift
+//  SpacePhoto
+//
+//  Created by Benjamin Reeps on 1/13/21.
+//
+
+import Foundation
+
+extension URL {
+    func withQueries(_ queries: [String: String]) -> URL? {
+        var components = URLComponents(url: self, resolvingAgainstBaseURL: true)
+        components?.queryItems = queries.map { URLQueryItem(name: $0.0, value: $0.1) }
+        return components?.url
+    }
+}
