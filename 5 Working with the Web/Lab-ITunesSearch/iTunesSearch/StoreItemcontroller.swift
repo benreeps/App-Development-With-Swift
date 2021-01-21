d186553f5f84954eefa15d13850edacc627ad737
Ben Reeps <benreeps@gmail.com>
2021-01-21 15:02:57 -0500

    add new file StoreItemController.swift

--- 5 Working with the Web/Lab-ITunesSearch/iTunesSearch/StoreItemcontroller.swift
+++ 5 Working with the Web/Lab-ITunesSearch/iTunesSearch/StoreItemcontroller.swift
@@ -7,3 +7,28 @@
 //
 
 import Foundation
+
+func fetchItems(matching query: [String: String], completion: @escaping ([StoreItem]?) -> Void) {
+
+    let baseURL = URL(string: "http://itunes.apple.com/search")!
+    guard let url = baseURL.withQueries(query) else {
+        completion(nil)
+        print("Unable to find url with queries provided.")
+        return
+    }
+    
+    let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
+        
+        let jsonDecoder = JSONDecoder()
+        if let data = data,
+        let storeItems = try? jsonDecoder.decode(StoreItems.self, from: data) {
+            completion(storeItems.results)
+           
+        } else {
+            print("Either no data was returned or data was not properly decoded")
+            completion(nil)
+            return
+        }
+    }
+    task.resume()
+}
