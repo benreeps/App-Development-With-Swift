8e86afe9c5c8d5b7bb9e247775a141b5e221713d
Ben Reeps <benreeps@gmail.com>
2021-01-23 17:21:53 -0500

    embed nav controllers in a tab bar controller

---
+++ 5 Working with the Web/GuidedProject-Restaurant/Restaurant/Restaurant/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Restaurant
+//
+//  Created by Benjamin Reeps on 1/23/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
