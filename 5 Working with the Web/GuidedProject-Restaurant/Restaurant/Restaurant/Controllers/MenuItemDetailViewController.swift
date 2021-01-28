60c80643ba8d39ddc7836b0e3a566eca3dbd68ca
Ben Reeps <benreeps@gmail.com>
2021-01-28 16:32:35 -0500

    set up outlets and assign label values

--- 5 Working with the Web/GuidedProject-Restaurant/Restaurant/Restaurant/Controllers/MenuItemDetailViewController.swift
+++ 5 Working with the Web/GuidedProject-Restaurant/Restaurant/Restaurant/Controllers/MenuItemDetailViewController.swift
@@ -9,23 +9,23 @@ import UIKit
 
 class MenuItemDetailViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var priceLabel: UILabel!
+    @IBOutlet weak var detailTextLabel: UILabel!
+    @IBOutlet weak var addToOrderButton: UIButton!
+    
     var menuItem: MenuItem!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        updateUI()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func updateUI() {
+        titleLabel.text = menuItem.name
+        priceLabel.text = String(format: "$%.2f", menuItem.price)
+        detailTextLabel.text = menuItem.detailText
     }
-    */
-
 }
